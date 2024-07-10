7e9cffe125e10a887e7a9bd4f0eaa09186cfc883
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-07-10 03:57:28 -0700

    Time: 102 ms (80.54%), Space: 17.3 MB (20.19%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -11,16 +11,19 @@ class Solution:
         have,need=0,len(countT)
         for r in range(len(s)):
             c = s[r]
-            windowcount[c]=1+windowcount.get(c,0)
-            if c in countT and windowcount[c]==countT[c]:
-                have+=1
+            #windowcount[c]=1+windowcount.get(c,0)
+            if c in countT:
+                windowcount[c]=1+windowcount.get(c,0)
+                if countT[c]==windowcount[c]:
+                    have+=1
             while have==need: #loop to find the minimum window and move left pointer by 1 towards right
                 if r-l+1<reslen:
                     reslen = r-l+1
                     res = [l,r]
-                windowcount[s[l]]-=1
-                if s[l] in countT and windowcount[s[l]]<countT[s[l]]:
-                    have-=1
+                if s[l] in windowcount:
+                    windowcount[s[l]]-=1
+                    if windowcount[s[l]]<countT[s[l]]:
+                        have-=1
                 l+=1
         
         return s[res[0]:res[1]+1] if reslen!=float('inf') else ""
