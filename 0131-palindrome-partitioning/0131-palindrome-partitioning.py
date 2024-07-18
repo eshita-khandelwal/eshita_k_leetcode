b93c36a02ae3b8ccf967076979fee1ff7ef5907a
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-07-17 19:09:58 -0700

    Time: 470 ms (42.96%), Space: 34.9 MB (74.83%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -3,11 +3,12 @@ class Solution:
         res = []
         part = []
         def ispal(s,i,j):
-            if i<=j and s[i]==s[j]:
-                i+=1
-                j-=1
-            else:
-                return False
+            for i in range(i,j+1):
+                if s[i]==s[j]:
+                    i+=1
+                    j-=1
+                else:
+                    return False
             return True
         def dfs(i):
             if i == len(s):
