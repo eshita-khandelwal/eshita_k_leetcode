e34720cb3d511f64d0cce6b57732c467add8e273
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-07-17 19:09:04 -0700

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.py
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.py
@@ -2,25 +2,22 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         res = []
         part = []
-        def isPal(s,i,j):
-            for k in range(i,j+1):
-                if s[i]==s[j]:
-                    i+=1
-                    j-=1
-                else:
-                    return False
+        def ispal(s,i,j):
+            if i<=j and s[i]==s[j]:
+                i+=1
+                j-=1
+            else:
+                return False
             return True
-
         def dfs(i):
             if i == len(s):
                 res.append(part.copy())
                 return
             for j in range(i,len(s)):
-                if isPal(s,i,j):
+                if ispal(s,i,j):
                     part.append(s[i:j+1])
                     dfs(j+1)
                     part.pop()
-        
         dfs(0)
         return res
-        
+                
