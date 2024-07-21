4203582c0738606e5d47a8eac6a3bd27f74534f3
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-07-21 11:52:34 -0700

    Time: 582 ms (58.37%), Space: 23.7 MB (16.77%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.py
@@ -1,15 +1,18 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        count = {}
-        res=0
-        oddc = 0
-        for i in range(len(nums)):
-            if nums[i]%2==1:
-                oddc+=1
-            count[oddc] = 1 + count.get(oddc,0)
-        if k in count:
-            res = count[k] 
-        for cnt,val in count.items():
-            if cnt+k in count:
-                res+=count[cnt] * count[cnt+k]
+        #we are using three pointer approach. l,r,m. initially l=m=0. we will calculate elements in a window that satisfy the condition as l-m+1. once we are in the window we will see increment m pointer till the start of first odd element. then the number of subarrays that can we formed will be m-l+1
+        l=m=0
+        res = 0
+        odd =0
+        for r in range(len(nums)):
+            odd +=nums[r]%2
+            while odd>k:
+                odd-=nums[l]%2
+                l+=1
+                m=l
+            if odd == k:
+                while nums[m]%2==0:
+                    m+=1
+                res+=m-l+1
         return res
+
