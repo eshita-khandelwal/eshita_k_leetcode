ebdcadb095e9311c83a5f13f26c69a26a9ec14c4
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-08-01 14:36:10 -0700

    Time: 28 ms (93.38%), Space: 16.6 MB (24.19%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,13 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums)==1:
-            return nums[0]
-        if len(nums)==2:
-            return max(nums[0],nums[1])
-        dp = [0]*len(nums)
+        #we can have choosen house1 or house 2 at the start
+        n = len(nums)
+        dp = [0 for i in range(n)]
         dp[0]=nums[0]
+        if n==1:
+            return nums[0]
         dp[1]=max(nums[0],nums[1])
         for i in range(2,len(nums)):
-            dp[i]=max(dp[i-1],dp[i-2]+nums[i])
-            
-        return max(dp[len(nums)-1],dp[len(nums)-2])
+            dp[i] = max(dp[i-2]+nums[i],dp[i-1])
+        return dp[n-1]
