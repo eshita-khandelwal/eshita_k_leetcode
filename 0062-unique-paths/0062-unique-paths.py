c9730ad5a5d0c3ea42397fef6a60aedfc666c42d
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-08-03 20:50:43 -0700

    Time: 36 ms (53.61%), Space: 16.5 MB (65.4%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,6 +1,8 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = [[0 for _ in range(m+1)] for _ in range(n+1)]
+        #to reach the finish point finish point will need 1 way to do that so we assign it to 0
+        #the last row or last col also has one way each to go and reach finish point as we can only move right or move down. we cannot move left or up. so assign them to 1. each inside boxes have ways -> or down. so we jsut add the ways and return dp[0][0]
+        dp = [[0 for _ in range(m)] for _ in range(n)]
         
         for i in range(n-1,-1,-1):
             for j in range(m-1,-1,-1):
