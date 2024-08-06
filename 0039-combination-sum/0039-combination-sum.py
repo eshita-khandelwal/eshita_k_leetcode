34f9d1f08bceca9603fe4029049046670dfc122b
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-08-06 07:35:21 -0700

    Time: 58 ms (33.42%), Space: 16.7 MB (34.76%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,18 +1,53 @@
 class Solution:
-    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
-        #https://www.youtube.com/watch?v=GBKI9VSKdGg
-        res = []
-        subset = []
-        def dfs(i):
-            if sum(subset) == target:
-                res.append(subset.copy())
-                return
-            if i == len(nums) or sum(subset)>target:
-                return
+   
+        # #https://www.youtube.com/watch?v=GBKI9VSKdGg
+        # res = []
+        # subset = []
+        # def dfs(i):
+        #     if sum(subset) == target:
+        #         res.append(subset.copy())
+        #         return
+        #     if i == len(nums) or sum(subset)>target:
+        #         return
             
-            subset.append(nums[i])
-            dfs(i) #call the fuction with i, keep calling it
-            subset.pop()
-            dfs(i+1) #you dont need nums[i] ever again so now move to next element. 
-        dfs(0)
+        #     subset.append(nums[i])
+        #     dfs(i) #call the fuction with i, keep calling it
+        #     subset.pop()
+        #     dfs(i+1) #you dont need nums[i] ever again so now move to next element. 
+        # dfs(0)
+        # return res
+
+
+    def combinationSum(self, nums: List[int], target: int) -> List[List[int]]:
+        res = []
+        subarr = []
+        def backtrack(i,s):
+            if s==target:
+                res.append(subarr.copy())
+                return
+            if s>target:
+                return
+            if i == len(nums):
+                return
+            subarr.append(nums[i])
+            s+=nums[i]
+            backtrack(i,s)
+            subarr.pop()
+            s-=nums[i]
+            backtrack(i+1,s)
+        backtrack(0,0)
         return res
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
