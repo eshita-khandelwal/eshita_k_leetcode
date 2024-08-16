53bdeafe128323fc1aedcd80a984d03df7e87f65
Eshita Khandelwal <32758473+eshita-khandelwal@users.noreply.github.com>
2024-08-15 18:56:48 -0700

    Time: 70 ms (67.74%), Space: 16.9 MB (96.19%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.py
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.py
@@ -1,12 +1,14 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        res=nums[0]
-        curMin,curMax = 1,1
-        for n in nums:
-            if n==0:
-                curMin,curMax = 1,1
-            temp = curMax
-            curMax = max(curMax*n,curMin*n,n)
-            curMin = min(temp*n,curMin*n,n)
-            res= max(res,curMax)
+        #pointers for this question: avoid 0 becasue it will make things bad, by resetting values of curmin and curmax to 1. maintain min,max at each i because there can be negative numbers too. 
+        res = float("-inf")
+        curmin = 1
+        curmax = 1
+        for i in nums:
+            if i==0:
+                curmin,curmax = 1,1
+            temp = curmax * i
+            curmax = max(i,temp,curmin*i)
+            curmin = min(i,temp,curmin*i)
+            res = max(res,curmax)
         return res
